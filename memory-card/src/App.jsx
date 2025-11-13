860ab3ac1e6f1e33bf3d6af839972be789548f89
Luis Sanchez <luisanchezdh@gmail.com>
2025-11-13 18:40:45 -0500

    fetching all pokemon data and images and displaying on page, added function to shuffle all pokemon data

--- memory-card/src/App.jsx
+++ memory-card/src/App.jsx
@@ -1,23 +1,45 @@
 import axios from "axios";
 import { useState, useEffect } from "react";
 import Cards from "./Components/Cards";
+import "./App.css";
 
 export default function App() {
-  const [data, setData] = useState(null);
+  const [pokemonData, setPokemonData] = useState([]);
   const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(null);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get("https://pokeapi.co/api/v2/pokemon/");
-        setData(response.data.results);
+        const response = await axios.get(
+          "https://pokeapi.co/api/v2/pokemon?limit=20"
+        );
+        const pokemonList = response.data.results;
+
+        //second API call to fetch pokemon images
+        const pokemonDetails = await Promise.all(
+          pokemonList.map((pokemon) => axios.get(pokemon.url))
+        );
+
+        //extract data from pokemon we need
+        const pokemonWithImages = pokemonDetails.map((res) => ({
+          name: res.data.name,
+          image: res.data.sprites.front_default,
+          id: res.data.id,
+        }));
+
+        setPokemonData(pokemonWithImages);
       } catch (error) {
         setError(error.message);
+      } finally {
+        setLoading(false);
       }
     };
 
     fetchData();
   }, []);
+  if (loading) return <div>Loading...</div>;
+  if (error) return <div>Error: {error}</div>;
 
-  return <Cards data={data} />;
+  return <Cards pokemonData={pokemonData} />;
 }
--- memory-card/src/Components/Cards.jsx
+++ memory-card/src/Components/Cards.jsx
@@ -1,9 +1,17 @@
-export default function Cards({ data }) {
-  console.log(data);
-
-  //   data.forEach((pokemon) => {
-  //     const pokemonName = pokemon.name;
-  //     const pokemonUrl = pokemon.url;
-  //     console.log(pokemonName, pokemonUrl);
-  //   });
+export default function Cards({ pokemonData }) {
+  console.log(pokemonData);
+  function shuffle(arr) {
+    arr.sort(() => Math.random() - 0.5);
+  }
+  shuffle(pokemonData);
+  return (
+    <div className="pokemonGrid">
+      {pokemonData.map((pokemon) => (
+        <div key={pokemon.id} className="pokemonCard">
+          <img src={pokemon.image} alt={pokemon.name}></img>
+          <h3>{pokemon.name}</h3>
+        </div>
+      ))}
+    </div>
+  );
 }
